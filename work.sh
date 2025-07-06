608b23786bc0ab3ecf26569ccea4c2609240c46c
xklstudio <57036669+xklstudio@users.noreply.github.com>
2025-07-06 11:31:01 +0800

    Delete work.sh

--- work.sh
+++
@@ -1,4 +0,0 @@
-
-CUDA_VISIBLE_DEVICES=1 python continual_train.py --logs-dir logs/label-0.1 --label_ratio 0.1  
-CUDA_VISIBLE_DEVICES=1 python continual_train.py --logs-dir logs/label-0.2 --label_ratio 0.2  
-CUDA_VISIBLE_DEVICES=1 python continual_train.py --logs-dir logs/label-0.5 --label_ratio 0.5  
